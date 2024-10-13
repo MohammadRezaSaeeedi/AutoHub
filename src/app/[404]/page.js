ff8b1f956c3cd396e0689c206f49d45edaca81d6
mohammadreza <m.r.saeedi2024@gmail.com>
2024-10-13 23:50:31 +0330

    add 404 page edit

---
+++ src/app/[404]/page.js
@@ -0,0 +1,11 @@
+import NoFound from "@/components/template/404";
+
+function NotFound404() {
+    return (
+        <div>
+            <NoFound />
+        </div>
+    )
+}
+
+export default NotFound404;
--- src/components/template/CategoryCard.jsx
+++ src/components/template/CategoryCard.jsx
@@ -14,8 +14,16 @@ function CategoryCard() {
     <div className="justify-center flex sm:mt-4 ">
       
     <div  className="flex justify-between  sm:gap-4 w-[160px] sm:w-[400px] md:w-[500px]  p-2 rounded-lg   " >
+      <Link href={`/category/${'hatchback'}`}>
+      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px] md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[18px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)]">
+        <span>Hatchback</span>
+        <div className="hidden sm:block " >
+          <Hatchback />
+        </div>
+      </div>
+      </Link>
       <Link href={`/category/${'sedan'}`}>
-      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px]  md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[18px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)] " >
+      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px]  md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[19px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)] " >
         <span>Sedan</span>
         <div className="hidden sm:block " >
           <Sedan />
@@ -23,18 +31,10 @@ function CategoryCard() {
       </div >
       </Link>
       <Link href={`/category/${'suv'}`}>
-      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px] md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[18px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)]">
+      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px] md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[19px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)]">
         <span>SUV</span>
         <div className="hidden sm:block " >
           <Suv />
-        </div>
-      </div>
-      </Link>
-      <Link href={`/category/${'hatchback'}`}>
-      <div className="flex flex-col sm:gap-2 w-[30px] sm:w-[80px] md:w-[110px] sm:font-bold items-center text-[5px] sm:text-[12px] md:text-[18px] text-white bg-emerald-300 p-2 rounded-md shadow-[0_10px_10px_rgba(0,0,0,0.1)]">
-        <span>Hatchback</span>
-        <div className="hidden sm:block " >
-          <Hatchback />
         </div>
       </div>
       </Link>
